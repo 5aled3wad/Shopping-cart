c64454ca926bd9aa3cb6af1f15907df6fb9a8763
Khaled Awad <5aled3wad>
2024-12-02 15:27:40 +0200

    [create signUp && login page]

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/signup", function (req, res, next) {
+  res.render("users/signup", { title: "userSignUp" });
 });
 
+router.post("/signup", (req, res, next) => {
+  res.send("OK");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("users/login", { title: "userLogin" });
+});
 module.exports = router;
