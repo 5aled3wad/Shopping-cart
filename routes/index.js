a3051f6bc1ad9f46bf657b28fcf0e670510e3f83
Khaled Awad <5aled3wad>
2024-12-02 14:30:16 +0200

    [read && display database]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
+const Products = require("../models/products");
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  Products.find()
+    .then((doc) => {
+      // to push 3 in one array [ [{},{},{}] , [{},{},{}] , [{},{},{}] ]
+      let productGrid = [];
+      let colGrid = 3;
+      for (let i = 0; i < doc.length; i += colGrid) {
+        productGrid.push(doc.slice(i, i + colGrid));
+      }
+      // console.log(productGrid);
+      res.render("index", { title: "Shopping-cart", products: productGrid });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
