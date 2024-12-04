dade2e7f2c6870aefa370d7dcf126b00c195270b
Khaled Awad <5aled3wad>
2024-12-04 21:05:29 +0200

    create [user-validator to login]

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,85 @@
 var express = require("express");
 const { validationResult } = require("express-validator");
 const userValidator = require("../validator/user_validator");
+const UserData = require("../models/users");
+const bcrypt = require("bcrypt");
 
 var router = express.Router();
 
-/* GET users listing. */
+// GET SignUp
 router.get("/signup", function (req, res, next) {
   res.render("users/signup", { title: "userSignUp", errorMessage: null });
 });
 
-router.post("/signup", userValidator, (req, res, next) => {
+// post SignUp
+router.post("/signup", userValidator.signUp, async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     // console.log(errors);
-    res.render("users/signup", {
-      title: "error",
+    return res.render("users/signup", {
+      title: "userSignUp",
       errorMessage: errors.array()[0].msg,
     });
   }
-  res.send("OK");
+  const saltRounds = 10;
+  const hashPassword = bcrypt.hashSync(req.body.password, saltRounds);
+
+  try {
+    const findEmail = await UserData.findOne({
+      email: req.body.email,
+    });
+    if (findEmail) {
+      return res.render("users/signup", {
+        title: "userSignUp",
+        errorMessage: "Invalid email or password",
+      });
+    }
+    const userData = new UserData({
+      email: req.body.email,
+      password: hashPassword,
+    });
+    userData.save();
+    res.render("users/login", { title: "userLogin", errorMessage: null });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+// get Login
 router.get("/login", (req, res, next) => {
-  res.render("users/login", { title: "userLogin" });
+  res.render("users/login", { title: "userLogin", errorMessage: null });
 });
+
+// post Login
+router.post("/login", userValidator.login, async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render("users/login", {
+      title: "userLogin",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+  const { email } = req.body;
+  const findUser = await UserData.findOne({
+    email: email,
+  });
+  if (!findUser) {
+    return res.render("users/login", {
+      title: "userLogin",
+      errorMessage: "Invalid email or password",
+    });
+  }
+  const matchPassword = bcrypt.compareSync(
+    req.body.password,
+    findUser.password
+  );
+  if (!matchPassword) {
+    return res.render("users/login", {
+      title: "userLogin",
+      errorMessage: "Invalid email or password",
+    });
+  }
+  res.render("users/profile", { title: "userProfile" });
+});
+
 module.exports = router;
--- validator/user_validator.js
+++ validator/user_validator.js
@@ -1,19 +1,26 @@
-const { body } = require("express-validator");
+const { check, body } = require("express-validator");
 
 // custom() use to compare between two values
 
 signUp = [
   // body('email').notEmpty().trim().withMessage('please enter your email'),
-  body("email").isEmail().trim().withMessage("email is not valid."),
+  body("email").isEmail().trim().withMessage("Invalid email."),
   body("password")
     .isLength({ min: 5 })
-    .withMessage("please enter password more than 5 char"),
+    .withMessage("Please enter password more than 5 char"),
   body("confirm-password").custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error("password and confirm-password not matching");
+      throw new Error("Password and confirm-password not matching");
     }
     return true;
   }),
 ];
 
-module.exports = signUp;
+login = [
+  check("email").isEmail().trim().withMessage("Invalid email."),
+  check("password")
+    .isLength({ min: 1 })
+    .withMessage("Please enter your password"),
+];
+
+module.exports = { signUp: signUp, login: login };
